c29ebaebee067042c86e5af485e3d2e24765ac17
jimmyday12 <jamesday@JDs-Macbook-Pro.brisbane.fusion.sport>
2021-03-12 12:57:11 +1100

    rebuilding site

--- docs/scripts/lms_fetch_detailed_stats.R
+++ docs/scripts/lms_fetch_detailed_stats.R
@@ -1,5 +1,4 @@
-
-fetch_detailed_stats <- function(id){
+get_dat_state <- function(id) {
   print(id)
   url <- paste0("https://admin.lastmanstands.com/api/v1/Fixtures/", id, "/LastManStands/FixtureState")
   resp <- httr::GET(url,
@@ -12,6 +11,12 @@ fetch_detailed_stats <- function(id){
   if(is.null(dat$State)) return(NULL)
   
   dat_state <- jsonlite::fromJSON(dat$State)
+}
+
+fetch_detailed_stats <- function(id){
+  
+  dat_state <- get_dat_state(id)
+  if(is.null(dat_state)) return(NULL)
 
   overs <- dat_state$Innings$Overs
   events <- overs %>%
@@ -44,6 +49,7 @@ fetch_detailed_stats <- function(id){
   batters_init <- bind_rows(strikers, nonstrikers) %>%
      filter(!is.na(Id))
   
+  
   get_batters <- function(x) {
     
     if(!"OutEvent" %in% names(x)) return(NULL)
@@ -71,6 +77,59 @@ fetch_detailed_stats <- function(id){
   # Get batters
   batters <- bind_rows(batters_init, batters_1, batters_2, 
                        batters_3, batters_4, batters_5) 
+  
+  out_events <- batters$OutEvent$BallResults
+  out_events[out_events %>% purrr::map_lgl(is.null)] <- NA
+  
+  slice_string <- function(df) {
+    if(!"$type" %in% names(df)) return(df)
+    if(any(str_detect(df$`$type`, "DoublePlay"))) return(slice_tail(df))
+    if(any(str_detect(df$`$type`, "Runs"))) return(slice_head(df))
+    if(any(str_detect(df$`$type`, "Steal"))) return(slice_head(df))
+  
+    return(df)
+  }
+  
+  out_events <- out_events %>% 
+    purrr::map(as_tibble) %>%
+    purrr::map_dfr(slice_string) %>%
+    rename(out_event_type = `$type`) %>%
+    mutate(out_event_type = str_remove(out_event_type, 
+                                       "live_scoring_core.dtos.outdoor_cricket.")) %>%
+    mutate(out_event_type = str_remove(out_event_type, "last_man_stands."))
+  
+  
+
+ # replace_outs <- function(out_events, string, new_string){
+  #  str_exists <- str_detect(out_events$out_event_type, string)
+  #  str_exists[is.na(str_exists)] <- FALSE
+  #  
+  #  if(any(str_exists)){
+  #    ind <- which(str_exists)
+  #    out_events$out_event_type[ind - 1] <- new_string
+  #  }
+  #  
+  #  return(out_events)
+  #}
+  
+  #out_events <- replace_outs(out_events, "DoublePlay", "DoublePlay")
+  #out_events <- replace_outs(out_events, "RunOut", "Run-Out")
+
+  #out_events <- out_events %>%
+  #  filter(!str_detect(out_event_type, "last_man_stands.DoublePlay") | is.na(out_event_type)) %>%
+#    filter(!str_detect(out_event_type, "Runs") | is.na(out_event_type)) %>%
+ #   filter(!str_detect(out_event_type, "last_man_stands.Steal") | is.na(out_event_type)) 
+    
+  
+  #print(paste0(
+  #  "Double Play exists: ", "DoublePlay" %in% out_events$out_event_type))
+  
+  out_events <- out_events %>%
+    mutate(Dismissal = out_event_type) %>%
+    select(Dismissal)
+  
+  #batters <- batters %>% bind_cols(out_events)
+  
   
   # Get bowlers
   bowlers_init <- events %>%
@@ -110,7 +169,15 @@ fetch_detailed_stats <- function(id){
     battingFirstName = dat_state$BattingFirst$Name,
     battingFirstId = dat_state$BattingFirst$Id,
     bowlingFirstName = dat_state$BowlingFirst$Name,
-    bowlingFirstId = dat_state$BowlingFirst$Id
+    bowlingFirstId = dat_state$BowlingFirst$Id,
+    battingFirstScoreRuns = dat_state$Innings$Score$Runs[1],
+    battingFirstScoreWickets = dat_state$Innings$Score$Wickets[1],
+    battingFirstScoreOvers = dat_state$Innings$Score$Overs$Over[1],
+    battingFirstScoreBalls = dat_state$Innings$Score$Overs$Ball[1], 
+    bowlingFirstScoreRuns = dat_state$Innings$Score$Runs[2],
+    bowlingFirstScoreWickets = dat_state$Innings$Score$Wickets[2],
+    bowlingFirstScoreOvers = dat_state$Innings$Score$Overs$Over[2],
+    bowlingFirstScoreBalls = dat_state$Innings$Score$Overs$Ball[2]
   )
   
   league_ids <- get_seas_league_ids(id)
